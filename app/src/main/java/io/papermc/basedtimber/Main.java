6d24b30c3648f7ae84c9ffe0503b348a3c8026dc
pairnon <janpairnon@gmail.com>
2024-05-18 22:55:32 -0700

    Register events

--- app/src/main/java/io/papermc/basedtimber/Main.java
+++ app/src/main/java/io/papermc/basedtimber/Main.java
@@ -1,5 +1,22 @@
 package io.papermc.basedtimber;
 
-public class Main {
-    
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+@SuppressWarnings("deprecation")
+public class Main extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().sendMessage("hello" + event.getPlayer().getDisplayName());
+    }
+
 }
