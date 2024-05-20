2ed73058e8d44cc42c3d7b3c937f0c2bfb58a0fe
pairnon <janpairnon@gmail.com>
2024-05-20 12:17:16 -0700

    Remove test join message

--- app/src/main/java/io/papermc/basedtimber/Main.java
+++ app/src/main/java/io/papermc/basedtimber/Main.java
@@ -9,7 +9,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-@SuppressWarnings("deprecation")
 public class Main extends JavaPlugin implements Listener {
 
     public static Material[] logMaterials = {Material.OAK_LOG, Material.SPRUCE_LOG, Material.BIRCH_LOG, Material.JUNGLE_LOG, Material.ACACIA_LOG, Material.DARK_OAK_LOG, Material.MANGROVE_LOG, Material.CHERRY_LOG};
@@ -28,7 +27,6 @@ public class Main extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
-        event.getPlayer().sendMessage("hello" + event.getPlayer().getDisplayName());
     }
 
 }
