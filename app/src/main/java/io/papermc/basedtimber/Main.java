2044f2889aec904c3c317d5d9f59561732e08f4c
pairnon <janpairnon@gmail.com>
2024-05-20 11:32:30 -0700

    Remove registerlog command

--- app/src/main/java/io/papermc/basedtimber/Main.java
+++ app/src/main/java/io/papermc/basedtimber/Main.java
@@ -2,7 +2,6 @@ package io.papermc.basedtimber;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -12,15 +11,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin implements Listener {
 
     public static Material[] logMaterials = {Material.OAK_LOG, Material.SPRUCE_LOG, Material.BIRCH_LOG, Material.JUNGLE_LOG, Material.ACACIA_LOG, Material.DARK_OAK_LOG, Material.MANGROVE_LOG, Material.CHERRY_LOG};
-    public static NamespacedKey ignoreLogKey = new NamespacedKey("basedtimber", "ignore");
 
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
 
         getServer().getPluginManager().registerEvents(new BlockBreakListener(), this);
-
-        this.getCommand("registerlog").setExecutor(new CommandRegisterLog());
     }
 
     @EventHandler
