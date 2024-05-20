310d1d0f63ca34ffc6f86a61aa4db55532511315
pairnon <janpairnon@gmail.com>
2024-05-20 12:16:43 -0700

    Implement toggling

--- app/src/main/java/io/papermc/basedtimber/BlockBreakListener.java
+++ app/src/main/java/io/papermc/basedtimber/BlockBreakListener.java
@@ -3,17 +3,23 @@ package io.papermc.basedtimber;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 
+@SuppressWarnings("deprecation")
 public class BlockBreakListener implements Listener {
 
     private static int maxTreeHeight = 31;
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
+
+        Player player = event.getPlayer();
+        if (!hasTimberEnabled(player)) { return; }
+
         Block brokenBlock = event.getBlock();
         for (Material log : Main.logMaterials) {
             if (brokenBlock.getType().equals(log)) {
@@ -25,6 +31,16 @@ public class BlockBreakListener implements Listener {
                 }
             }
         }
+    }
+
+    private boolean hasTimberEnabled(Player player) {
+        String displayname = player.getDisplayName();
+        for (String s : Main.timberEnabledPlayers) {
+            if (s.equals(displayname)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private void dropItem(Block aboveBlock) {
